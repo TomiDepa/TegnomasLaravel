026e0a6a1e76e41009d4ba2558ac17c2e706cdbb
Tomas De Paulo <tomas.depaulo@davinci.edu.ar>
2023-11-27 13:33:22 -0300

    Revert "Revert "Merge pull request #15 from TomiDepa/julian""
    This reverts commit 693b7f644b9032a4ad349497386e390d0237c760.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Support\Facades\Storage;
+
 
 class PostController extends Controller
 {
@@ -30,8 +32,19 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
-        Post::create($request->all());
+        $data = $request->all();
+        // logica para la carga de imagen 
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('post_images', 'public');
+            $data['image_path'] = $imagePath;
+        }
+        Post::create($data);
         return redirect(route('posts.index'));
+       /* Post::create($request->all());
+        return redirect(route('posts.index'));*/
+
+        // logica para la carga de imagen 
+        
     }
 
     /**
@@ -55,8 +68,23 @@ class PostController extends Controller
      */
     public function update(UpdatePostRequest $request, Post $post)
     {
-        $post->update($request->all());
+        $data = $request->all();
+        
+        // logica para la nueva imagen
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('post_images', 'public');
+            $data['image_path'] = $imagePath;
+
+            // Eliminar la imagen anterior si existe
+            if ($post->image_path) {
+                // Asegúrarse de que sea eliminada la foto anteior 
+                Storage::disk('public')->delete($post->image_path);
+            }
+        }
+        $post->update($data);
         return redirect()->route('posts.index');
+       /* $post->update($request->all());
+        return redirect()->route('posts.index');*/
     }
 
     /**
@@ -64,6 +92,11 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        // elminar iamgen asociada antes de eliminar el post
+        if ($post->image_path) {
+            Storage::disk('public')->delete($post->image_path);
+        }
+
         $post->delete();
         return redirect()->route('posts.index');
     }
